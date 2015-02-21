934a76ff8a2d4f9dc409caead4e1ea0112bc0a63
Sang Woon Lee <sangwoon@usc.edu>
2015-02-21 03:54:08 -0800

    Attract

--- Redcop/Assets/Capsule/PlayerMovement.cs
+++ Redcop/Assets/Capsule/PlayerMovement.cs
@@ -1,36 +1,48 @@
 using UnityEngine;
 using System.Collections;
-//test
+
 
 public class PlayerMovement : MonoBehaviour
 	
 {
 	private Vector3 movementVector;
 	private CharacterController characterController;
-	private float movementSpeed = 8;
+	private float movementSpeed = 5;
 	private float jumpPower = 15;
 	private float gravity = 40;
+	public int time = 30;
 	
 	
 	void Start()
 	{
 		characterController = GetComponent<CharacterController>();
+		StartCoroutine (countdown ());
 	}
 	
+	IEnumerator countdown(){
+		while (time > 0) {
+			movementVector.z -= movementSpeed;
+			yield return new WaitForSeconds(60);
+			time -=2;
+		}
+	}
 	
 	void Update()
 	{
+		if (movementVector.y > 0) {
+			movementVector.y = 0; 
+		}
 		movementVector.x = 0;
-		movementVector.z = 0;
-		if(Input.GetButtonDown("A"))
+		if(Input.GetButtonDown("left"))
 		{
 			movementVector.x += 10*movementSpeed;
 		}
-		if(Input.GetButtonDown("D"))
+		if(Input.GetButtonDown("right"))
 		{
 			movementVector.x -= 10*movementSpeed;
 		}
-
+		
+		
 		characterController.Move(movementVector * Time.deltaTime);
 		
 	}
--- Redcop/Assets/Capsule/PlayerMovement2.cs
+++ Redcop/Assets/Capsule/PlayerMovement2.cs
@@ -7,21 +7,32 @@ public class PlayerMovement2 : MonoBehaviour
 {
 	private Vector3 movementVector1;
 	private CharacterController characterController1;
-	private float movementSpeed = 8;
+	private float movementSpeed = 5;
 	private float jumpPower = 15;
 	private float gravity = 40;
+	public int time = 30;
 	
 	
 	void Start()
 	{
 		characterController1 = GetComponent<CharacterController>();
+		StartCoroutine (countdown ());
 	}
-	
+
+	IEnumerator countdown(){
+		while (time > 0) {
+			movementVector1.z += movementSpeed;
+			yield return new WaitForSeconds(60);
+			time -=2;
+		}
+	}
 	
 	void Update()
 	{
+		if (movementVector1.y > 0) {
+			movementVector1.y = 0; 
+		}
 		movementVector1.x = 0;
-		movementVector1.z = 0;
 		if(Input.GetButtonDown("left"))
 		{
 			movementVector1.x -= 10*movementSpeed;
